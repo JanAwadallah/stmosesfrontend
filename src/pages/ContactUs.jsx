55d62a539dd60237fc0bd73c2e70e386ab845d54
Jan Awadallah <jan.awadallah@gmail.com>
2022-10-16 18:18:08 +1100

    Update ContactUs.jsx

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -3,9 +3,9 @@ import Form from "../components/Form";
 
 const ContactUs = () => {
   return (
-    <div className="container d-flex justify-content-center mt-3">
+   
       <Form />
-    </div>
+  
   );
 };
 
