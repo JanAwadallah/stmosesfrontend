39af3fad09f6150e5041a65177aa6d6ba33e6b47
Jan Awadallah <jan.awadallah@gmail.com>
2022-10-17 11:25:37 +1100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import ArticleCard from "../components/ArticleCard";
 import Slider from "../components/Slider";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Spinner from "../components/Spinner";
 
 const Home = () => {
   const navigate = useNavigate();
