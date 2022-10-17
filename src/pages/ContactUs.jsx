0159bbb9347e188b24373f7f102229c8604cab6a
Jan Awadallah <jan.awadallah@gmail.com>
2022-10-17 14:00:09 +1100

    Update ContactUs.jsx

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -4,10 +4,10 @@ import Form from "../components/Form";
 const ContactUs = () => {
   return (
    
-      <div style={{maxWidth:"700px", display:"flex", justifyContent:"center"}}>
+     
     
-      <Form />
-   </div>
+      <Form style={{maxWidth:"700px"}} />
+   
   
   );
 };
